180833b2a7fa15aa4fdb31f77a15f83eccdf5969
Piotr Grędowski <piotrgredowski@gmail.com>
2022-07-20 07:33:51 +0200

    Add some sample python code

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def get_team_name():
+    """Function returning name of team which will win Python Code Jam 2022
+
+    Returns:
+        str: name of best team
+    """
+    return "Manticores"
+
+
+if __name__ == "__main__":
+    print(f"Hello {get_team_name()}!")
