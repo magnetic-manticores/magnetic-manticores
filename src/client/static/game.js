3cf221b94e974dceb696729271edfe0b532232dc
Luca Huelle <30503695+tameTNT@users.noreply.github.com>
2022-07-24 16:51:34 +0300

    💄 Install eslint and style js files
    Actually be consistent with the semi-colons basically.

--- src/client/static/game.js
+++ src/client/static/game.js
@@ -4,8 +4,8 @@ export const addJoinButton = function(wsEvent, parentDiv) {
   const joinLink = `${window.location.href}?joinKey=${wsEvent.joinKey}`;
   const joinButton = document.querySelector("#joinButton");
   joinButton.hidden = false;
-  joinButton.addEventListener('click', (e) => {
+  joinButton.addEventListener("click", () => {
     navigator.clipboard.writeText(joinLink).then();
   });
   parentDiv.appendChild(joinButton);
-}
+};
--- src/client/static/index.js
+++ src/client/static/index.js
@@ -1,10 +1,10 @@
-import { addJoinButton } from "./game.js"
+import { addJoinButton } from "./game.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   // Misc. loading things
   // Enable Bootstrap popovers
-  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+  const popoverTriggerList = document.querySelectorAll("[data-bs-toggle=\"popover\"]");
+  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
   // Websocket things
   const websocket = new WebSocket("ws://localhost:8001");
@@ -13,7 +13,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // Register event listeners to communicate with websocket
   initGame(websocket);
   receiveMessages(websocket);
-})
+});
 
 // Sends initial init event to websocket to create/join a new game
 function initGame(websocket) {
@@ -28,7 +28,7 @@ function initGame(websocket) {
     }
     // Otherwise, the first player is starting a new game
     websocket.send(JSON.stringify(event));
-  })
+  });
 }
 
 // Handles all events/messages received from websocket
@@ -37,42 +37,41 @@ function receiveMessages(websocket) {
     // parse incoming websocket message to JSON
     const event = JSON.parse(data);
     switch (event.type) {  // proceed according to event type
-      case "init":  // initial game creation; make join/share button
-        const lobbyDiv = document.querySelector(".lobby-actions");
-        addJoinButton(event, lobbyDiv);
-        break;
-      case "startReady":  // both players are in the lobby and waiting to start
-        break;
-      case "startGame": // both players connected and ready to start game:
-        // allocate roles and change live view
-        break;
-      case "codeUpdate":  // update displayed text/code in editor
-        break;
-      case "editLimit":  // editing character limit reached for bugger:
-        // revert to given file edit state & inform player
-        break;
-      case "timeUp":  // round time expired; ready to move to next round
-        break;
-      case "testingResults":  // results from code testing to be shown
-        break
-      case "error":  // any error or exception thrown by game logic
-        showAlert(`An error occurred: ${event.message}. Please refer to websocket console.`);
-        break;
+    case "init":  // initial game creation; make join/share button
+      addJoinButton(event, document.querySelector(".lobby-actions"));
+      break;
+    case "startReady":  // both players are in the lobby and waiting to start
+      break;
+    case "startGame": // both players connected and ready to start game:
+      // allocate roles and change live view
+      break;
+    case "codeUpdate":  // update displayed text/code in editor
+      break;
+    case "editLimit":  // editing character limit reached for bugger:
+      // revert to given file edit state & inform player
+      break;
+    case "timeUp":  // round time expired; ready to move to next round
+      break;
+    case "testingResults":  // results from code testing to be shown
+      break;
+    case "error":  // any error or exception thrown by game logic
+      showAlert(`An error occurred: ${event.message}. Please refer to websocket console.`);
+      break;
     }
-  })
+  });
 }
 
 function sendEvent(event, websocket) {
   // Only send the event if the websocket is still open
-    if (websocket.readyState !== websocket.OPEN) {
-      showAlert("The websocket is no longer open. Please refer to the websocket console.");
-    } else {
-      websocket.send(JSON.stringify(event));
-    }
+  if (websocket.readyState !== websocket.OPEN) {
+    showAlert("The websocket is no longer open. Please refer to the websocket console.");
+  } else {
+    websocket.send(JSON.stringify(event));
+  }
 }
 
 function showAlert(msg) {
   window.setTimeout(() => {
-    window.alert(msg)
+    window.alert(msg);
   }, 50);
 }
